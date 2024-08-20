88236c3d9e8eb4dad5d9b24a6b60257aa747f083
Joyce Oh <105008251+joyceoh@users.noreply.github.com>
2024-08-19 20:28:52 -0400

    Revert "Fix Extension boilerplate"

--- extension/node_modules/undici-types/retry-agent.d.ts
+++
@@ -1,8 +0,0 @@
-import Dispatcher from 'undici-types/dispatcher'
-import RetryHandler from 'undici-types/retry-handler'
-
-export default RetryAgent
-
-declare class RetryAgent extends Dispatcher {
-  constructor(dispatcher: Dispatcher, options?: RetryHandler.RetryOptions)
-}
