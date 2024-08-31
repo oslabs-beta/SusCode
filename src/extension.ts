09d1904a9eb1988ece3da0bce3daac871dd906f2
Ali McNamara <allisonrm95@gmail.com>
2024-08-31 14:40:40 -0400

    renamed scanWindow as openResultPanel, renamed extensionScan as filepath, added comments that explain functionality

--- src/extension.ts
+++ src/extension.ts
@@ -19,8 +19,11 @@ const nonce = getNonce();
 //this function gets invoked when the extension is ran/opened
 //A view provider is required to have a webview in the sidebar, so we create a class down below and then initialize new instance of the class  within activat() called ExtensionsSidebarViewProvider
 //We register the sidebar webview provider and assign it to the variable 'sidebar'
-//register suscode.displayExtensions command that invokes getExtensions() to get the extension names and filepaths as a nested array, assigns that to 'extensionsList', and then invokes the displayExtensions method on ExtensionsSidebarViewProvider
-//displayExtensions() posts a message to the sidebar with the type 'displayExtensions' and sends the extensionsList as the value of the message. This is received in sidebarApp.tsx
+//We register suscode.displayExtensions command that invokes getExtensions() to get the extension names and filepaths as a nested array, assigns that to 'extensionsList', and then invokes the displayExtensions method on ExtensionsSidebarViewProvider
+//Then displayExtensions() posts a message to the sidebar with the type 'displayExtensions' and sends the extensionsList as the value of the message. This is received in sidebarApp.tsx
+//We register suscode.openResultPanel command which when executed will invoke the anonymous function that takes a 'filepath' parameter to create a webview and assign it to the variable 'panel'
+//----> we use getPanelHTML() to locate the panelIndex.html, read the contents, create a path to the bundled file 'panel.js' and replace the script with a URI to the bundle, as well as replace nonce with the variable defined globally on this file
+//----> we take the passed in filepath and use slice(1, -1) to remove extra quotation marks before invoking reader(filepath, panel), which is imported from fileFinder.ts
 export function activate(context: vscode.ExtensionContext) {
   const provider = new ExtensionsSidebarViewProvider(context.extensionUri);
 
@@ -29,12 +32,9 @@ export function activate(context: vscode.ExtensionContext) {
     provider
   );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('suscode.displayExtensions', () => {
-      console.log('Command "suscode.displayExtensions" executed');
-      const extensions = getExtensions();
-      provider.displayExtensions(extensions);
-
+  const displayExtensions = vscode.commands.registerCommand(
+    'suscode.displayExtensions',
+    () => {
       function getExtensions() {
         const extensions = vscode.extensions.all.filter(
           (extension) => !extension.id.startsWith('vscode.')
@@ -44,25 +44,18 @@ export function activate(context: vscode.ExtensionContext) {
           let extensionPath = JSON.stringify(extensionObj.extensionUri.path);
           return [displayName, extensionPath];
         });
-        //check if we need spread and array
-        return [...extensionsList];
+        return extensionsList;
       }
-    })
+      const extensions = getExtensions();
+      provider.displayExtensions(extensions);
+    }
   );
 
-  // Automatically execute the 'suscode.displayExtensions' command when the extension is activated
-  //may not need
-  vscode.commands.executeCommand('suscode.displayExtensions');
-
-  ////////////////////////////////////////////////////////
-  // Register a command to create a new webview panel to display scan results
-  ////////////////////////////////////////////////////////
   const openResultPanel = vscode.commands.registerCommand(
     'suscode.openResultPanel',
-    (extensionScan) => {
-      // Create a new webview panel to display the results
+    (filepath) => {
       const panel = vscode.window.createWebviewPanel(
-        'extensionScans',
+        'resultPanel',
         'SusCode Results',
         vscode.ViewColumn.One,
         {
@@ -97,43 +90,46 @@ export function activate(context: vscode.ExtensionContext) {
 
       panel.webview.html = getPanelHTML();
 
-      extensionScan = extensionScan[0].slice(1, -1);
-      console.log(extensionScan);
-      reader(extensionScan, panel);
+      filepath = filepath[0].slice(1, -1);
+      reader(filepath, panel);
     }
   );
 
-  context.subscriptions.push(openResultPanel, sidebar);
+  context.subscriptions.push(sidebar, displayExtensions, openResultPanel);
 }
+
+//=========================ExtensionsSidebarViewProvider==================================================//
+//this class implements a webview view provider, allowing us to create a webview in the sidebar ('explorer')
+// it starts by setting the viewType, which is found in package.json under views, then handles some vscode API private labeling and establishes a URI to the webviewView with the label _extensionURI
+// the resolveWebviewView() initializes the webview, sets the context and creates a _view variable, before setting the classic options to enable scripts and set localSourceRoots
+//----> this function also uses getWebviewHTML (defined outseid of the resolveWebviewFunction) and sets the evaluated result as the html for the webview
+//----> this function also establishes an event listener:
+//window.addEventListener listens for 'getExtensions' coming from sidebarApp.tsx when a user clicks the 'get extensions' button, and will execute the suscode.displayExtension command
+//window.addEventListener also listens for 'extensionSelected' coming from sidebarApp.tsx when a user clicks/selects an extension, and will execute the suscode.openResultPanel command passing in the filepath which was the value of the message it received.
+//getWebviewHTML locates the sidebarIndex.html, reads the contents, creates a path to the bundled file 'sidebar.js' and replace the script with a URI to the bundle, as well as replace nonce with the variable defined globally on this file
+//Finally we export the default function deactivate() which shuts down the extension functionality when it is closed.
 
 class ExtensionsSidebarViewProvider implements vscode.WebviewViewProvider {
   public static readonly viewType = 'ExtensionsSidebarViewProvider';
-
   private _view?: vscode.WebviewView;
   private _isInitialized = false;
-
   constructor(private readonly _extensionUri: vscode.Uri) {}
 
-  // Method to resolve and initialize the webview when it is displayed
   public resolveWebviewView(
     webviewView: vscode.WebviewView,
     context: vscode.WebviewViewResolveContext,
     _token: vscode.CancellationToken
   ) {
-    console.log('WebviewView is being resolved');
     this._view = webviewView;
     this._isInitialized = true;
 
-    // Set webview options, including enabling scripts and setting local resource roots
     webviewView.webview.options = {
       enableScripts: true,
       localResourceRoots: [this._extensionUri],
     };
 
-    // Set the HTML content for the webview
     webviewView.webview.html = this.getWebviewHTML(webviewView.webview);
 
-    // Handle messages received from the webview
     webviewView.webview.onDidReceiveMessage((data) => {
       console.log('Message received:', data);
       switch (data.type) {
@@ -149,20 +145,6 @@ class ExtensionsSidebarViewProvider implements vscode.WebviewViewProvider {
       }
     });
   }
-
-  public displayExtensions(extensions: any[] | undefined) {
-    if (this._isInitialized && this._view) {
-      // Show the webview and send the extensions data
-      this._view.show?.(true);
-      this._view.webview.postMessage({
-        type: 'displayExtensions',
-        value: extensions,
-      });
-    } else {
-      console.error('Webview is not initialized');
-    }
-  }
-
   private getWebviewHTML(webview: vscode.Webview) {
     const htmlPath = path.join(
       this._extensionUri.fsPath,
@@ -178,7 +160,6 @@ class ExtensionsSidebarViewProvider implements vscode.WebviewViewProvider {
       )
     );
 
-    // Replace placeholders in the HTML with actual URIs and nonce
     htmlContent = htmlContent
       .replace(/\${nonce}/g, nonce)
       .replace(/\${scriptUri}/g, scriptUri.toString());
@@ -186,6 +167,18 @@ class ExtensionsSidebarViewProvider implements vscode.WebviewViewProvider {
     console.log('Generated HTML Content:', htmlContent);
 
     return htmlContent;
+  }
+
+  public displayExtensions(extensions: any[] | undefined) {
+    if (this._isInitialized && this._view) {
+      this._view.show?.(true);
+      this._view.webview.postMessage({
+        type: 'displayExtensions',
+        value: extensions,
+      });
+    } else {
+      console.error('Webview is not initialized');
+    }
   }
 }
 
