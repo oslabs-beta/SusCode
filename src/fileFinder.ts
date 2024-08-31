9438e881d42839659ab25752a9fb3992c441cbee
Joyce Oh <joyce.oh@live.com>
2024-08-31 12:43:16 -0400

    finialize isolated code now dynamic for all JS files still needs to connect to fileReader at final phase of cleanup

--- src/fileFinder.ts
+++ src/fileFinder.ts
@@ -5,47 +5,36 @@ import { WebviewPanel } from 'vscode';
 
 export function reader(extPath: string, panel: WebviewPanel):void {
   // makes list of items in main directory
-  console.log('inside fileFinder');
-  console.log('dirArr: ', extPath);
   const dirArr = fs.readdirSync(extPath);
   // make an array with all the file path to be checked
   const regexJs = /.jsx?$/i;
-  // using set
-  // const pathFoundFiles = new Set<string>();
-  const pathFoundFiles:string[] = [];
-  console.log('array of found files', pathFoundFiles);
-
+  
   //helper function traversing the given directory
   const findingExtension = (foldArr: string[], extenPath:string):void => {
-    //const extensionRegex = new RegExp('extensions?','gi');
-    // if dir arr has extension file we go into condition
-    // if (foldArr.some(el => el.match(/.jsx?$/i))) {
-    //   const toBeTested = foldArr.filter(el => el.match(/.jsx?$/i));
-    //   for (let file of toBeTested){
-    //     pathFoundFiles.push(path.join(extenPath, file));
-    //   }
-    // }
-    // traversing to find extension files
-    // exclude folders and files we don't need to analyze // dynamic: do so for all files in the gitignore
-    foldArr.filter(item => item !== 'node_modules' && item[0] !== '.');
-        //console.log('inside some', Array.from(pathFoundFiles));
-    if (foldArr.some(el => el.match(/.jsx?$/i))) {
-      const toBeTested = foldArr.filter(el => el.match(/.jsx?$/i));
-      console.log('toBeTested', toBeTested);
+    // exclude folders and files we don't need to analyze
+    foldArr = foldArr.filter(item => item !== 'node_modules' && item[0] !== '.');
+
+    // finding JS files and sending it to fileReader
+    if (foldArr.some(el => el.match(regexJs))) {
+      const pathFoundFiles:string[] = [];
+      const toBeTested = foldArr.filter(el => el.match(regexJs));
       for (let file of toBeTested){
-        // pathFoundFiles.add(path.join(extenPath, file));
-        pathFoundFiles.push(path.join(extenPath, file));
+        pathFoundFiles.push(path.join(extenPath, file)); 
       }
+      // ***** remove console.log and add the test once we combine cleanup *****
+      console.log('path found files', pathFoundFiles);
+      //streamFilesInDirectory(pathFoundFiles, panel);
     }
+
     //checking for directories
     for (let item of foldArr) {
-      console.log('inside loop', item)
       const insideExtension = path.join(extenPath, item);
       fs.lstat(insideExtension, (err: unknown, stats: { isDirectory: () => boolean; }) => {
         if (err) {
           console.log('error while finding file. ', err);
         } else {
           if (stats.isDirectory()) {
+            // open up the folder and recurse through helper function
             const nextFolderArr = fs.readdirSync(insideExtension);
             findingExtension(nextFolderArr, insideExtension);
           }
@@ -53,13 +42,7 @@ export function reader(extPath: string, panel: WebviewPanel):void {
         }
       });
     }
-    return;
   };
-
   findingExtension(dirArr, extPath);
-  // once get all the file path we want to test give it to tester funcs
-  console.log('here');
-  console.log('pathfoundfiles', Array.from(pathFoundFiles));
-  //streamFilesInDirectory(pathFoundFiles, panel);
 
 }
