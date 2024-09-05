073c880dca0208a2d0502082c31254d9961cc48d
Ali McNamara <allisonrm95@gmail.com>
2024-09-05 19:54:27 -0400

    playing with mui

--- src/panel/panelApp.tsx
+++ src/panel/panelApp.tsx
@@ -2,12 +2,20 @@ import * as React from 'react';
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 function App() {
   // initialize state for the read me description
   const [readMe, setReadMe] = useState<string>('');
+  const [names, setNames] = useState<string[]>([
+    'Liveshare',
+    'quokka',
+    'postman',
+  ]);
+  const [value, setValue] = useState<number>(0);
 
   //====================   LISTENING FOR MESSAGES FROM findReadMe() WITHIN findReadMe.ts   =================================//
   //assigning the data sent to the semantic variable message
@@ -30,65 +38,75 @@ function App() {
   //if the message type is 'update' we add a p tag to display the text which is all the occurence of found patterns
   //if the message type is 'end' we add a line, a bolded declaration that the file is finished reading, and another line
   //if the message type is 'error' we display a p tag of red text delcaring the file name and error message
-  window.addEventListener('message', (event) => {
-    const contentDiv: any = document.getElementById('content');
-    const message = event.data;
-    switch (message.type) {
-      case 'update': {
-        contentDiv.innerHTML += '<p>' + message.text + '</p>';
-        break;
-      }
-      case 'end': {
-        contentDiv.innerHTML +=
-          '<hr/><strong>Finished reading ' +
-          message.fileName +
-          '</strong><hr/>';
-        break;
-      }
-      case 'error': {
-        contentDiv.innerHTML +=
-          '<p style="color:red;">' + message.text + '</p>';
-        break;
-      }
-    }
+  // window.addEventListener('message', (event) => {
+  //   const message = event.data;
+  //   const contentDiv: any = document.getElementById(
+  //     `panelFor${message.fileName}`
+  //   );
+  //   // setNames(message.fileName);
+  //   switch (message.type) {
+  //     case 'update': {
+  //       setNames(message.fileName);
+  //       contentDiv.innerHTML += '<p>' + message.text + '</p>';
+  //       break;
+  //     }
+  //     case 'end': {
+  //       contentDiv.innerHTML +=
+  //         '<hr/><strong>Finished reading ' +
+  //         message.fileName +
+  //         '</strong><hr/>';
+  //       break;
+  //     }
+  //     case 'error': {
+  //       contentDiv.innerHTML +=
+  //         '<p style="color:red;">' + message.text + '</p>';
+  //       break;
+  //     }
+  //   }
+  // });
+  let values = ['0', '1', '2'];
+  const tabs = names.map((extensionName, i) => {
+    return <Tab label={extensionName} value={values[i]} />;
   });
 
-  return (
-    <Box
-      sx={{
-        width: '100vw',
-        m: 3,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
-      <Box
-        sx={{
-          width: '450px',
-          m: 2,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Typography variant='h4' fontWeight='bold'>
-          SusCode Extension Scan Results: {readMe}
-        </Typography>
-      </Box>
+  // const tabPanels = names.map((extensionName, i: number) => {
+  //   let content = `panelFor${extensionName}`;
+  //   return (
+  //     <TabPanel value={i.toString()} id={content}>
+  //       Scan results for: {extensionName}
+  //     </TabPanel>
+  //   );
+  // });
 
-      <Box>
-        <Tabs value={0}>
-          <Tab label='Scan Result:' />
-        </Tabs>
-        <Box sx={{ padding: 2 }}>
-          <Box id='content'></Box>
-          <Typography variant='body1' fontWeight='light'></Typography>
+  // const tabPanelsFake = names.map((extensionName, i: number) => {
+  //   return (
+  //     <TabPanel value={i.toString()}>
+  //       Scan results for: {extensionName}
+  //       <Typography> eval() was found 53 times in extension.js</Typography>
+  //     </TabPanel>
+  //   );
+  // });
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label='lab API tabs example'>
+            {tabs}
+          </TabList>
         </Box>
-      </Box>
+        <TabPanel value='0'>Item One</TabPanel>
+        <TabPanel value='1'>Item Two</TabPanel>
+        <TabPanel value='2'>Item Three</TabPanel>
+      </TabContext>
     </Box>
   );
 }
 
 export default App;
+//        {tabPanelsFake}
+// {tabPanels}
