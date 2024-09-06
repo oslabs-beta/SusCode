9ce44d266d5abb1e469f1671b38cfe1ea4c199e5
Seth Hollingsworth <>
2024-09-06 12:52:47 -0700

    final sidebar cleanup

--- src/sidebar/sidebarIndex.tsx
+++ src/sidebar/sidebarIndex.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './sidebarApp';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2', // Primary color for the button
-    },
-  },
-});
-
-console.log('ReactDOM.createRoot executed');
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <ThemeProvider theme={theme}>
-      <CssBaseline />
+      <CssBaseline>
       <App />
-    </ThemeProvider>
+      </CssBaseline>
 );
